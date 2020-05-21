3c21091ccb7ff549617f0762ccb784d85198a07d
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-21 11:43:06 +0530

    Added LastName Validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -6,15 +6,28 @@ echo "Welcome to User Registration program"
 
 	echo "A reges pattern to Check The Firs Name"
 	echo "Enter the name you want to check"
-	read name
+	read FirstName
 
 	pat="^[A-Z]{1}[^A-Z][a-z]"
 
-	if [[ $name =~ $pat ]]
+	if [[ $FirstName =~ $pat ]]
 	then
 		echo Valid
 	else
 		echo Invalid
 
 	fi
+#-------------------New Branch UC2 --------------------------
 
+
+	echo "Checking Last Name"
+	echo "Enter Last Name"
+
+	read LastName
+
+	if [[ $LastName =~ $pat ]]
+	then
+		echo "valid LastName"
+	else
+		echo  "Invalid LastName"
+	fi
