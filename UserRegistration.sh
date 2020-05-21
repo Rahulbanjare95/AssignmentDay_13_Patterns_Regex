7cd4ec9c6f96be6267152e7fd4cd9eb8b3b0a563
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-21 11:29:31 +0530

    Added Feature to match FirstName Starting with Upper case

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,1 +1,20 @@
+#!/bin/bash -x
 echo "Welcome to User Registration program"
+
+#-------------New Branch UC1 -------------------
+
+
+	echo "A reges pattern to Check The Firs Name"
+	echo "Enter the name you want to check"
+	read name
+
+	pat="^[A-Z]{1}[^A-Z][a-z]"
+
+	if [[ $name =~ $pat ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+
+	fi
+
